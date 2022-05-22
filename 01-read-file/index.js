94b50cdce9438bd13092b3abfea85f8112199b22
AnnYablonskaya <yaanna1910@gmail.com>
2022-05-22 21:48:23 +0300

    feat: add 01 and 02

---
+++ 01-read-file/index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+const text = fs.createReadStream(path.join(__dirname, 'text.txt'));
+text.on('data', (data) => {
+  console.log(data.toString());
+});
---
+++ 02-write-file/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const {stdin, stdout} = process;
+const text = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+stdout.write('Hi! Enter your thoughts...\n');
+
+stdin.on ('data', data => {
+  if (data.toString().trim() === 'exit'){
+    stdout.write('Hmm. Bye! Come back!\n');
+    process.exit(); 
+  }
+  text.write(data);
+  
+});
+  
+process.on('SIGINT', () => {
+  stdout.write('Hmm. Bye! Come back!\n');
+  process.exit();
+});
